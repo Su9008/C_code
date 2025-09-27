804e8442d489a09c52ce12ee13193f90423ae5d0
Su9008 <danny90825@gmail.com>
2025-09-27 11:24:09 +0800

    Update test.c

--- binary_search/binary_search/test.c
+++ binary_search/binary_search/test.c
@@ -10,7 +10,9 @@ int search(int* nums, int numsSize, int target) {
     int right = numsSize - 1;
     while (left <= right)
     {
-        int mid = (left + right) / 2;
+        // int mid = (left + right) / 2; 
+		int mid = left + (right - left) / 2;
+		
         if (nums[mid] < target)
         {
             left = mid + 1;
@@ -22,7 +24,6 @@ int search(int* nums, int numsSize, int target) {
         else
         {
             return mid;
-            break;
         }
     }
     return -1;
